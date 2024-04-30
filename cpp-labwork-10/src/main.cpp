628bb41f063eb26e03d15c6ed47da0b1abe1fdc2
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2024-04-30 18:46:43 +0300

    Removed env variable, moved to config

--- cpp-labwork-10/src/main.cpp
+++ cpp-labwork-10/src/main.cpp
@@ -7,33 +7,23 @@ int main() {
   std::ifstream file("config.json");
 
   if (!file) {
-    std::cout << "config.json file not found" << "\n";
+    std::cout << "config.json file not found"
+              << "\n";
     return 2;
   }
 
   json f = json::parse(file);
 
   if (f.empty()) {
-    std::cout << "error while parsing json" << "\n";
+    std::cout << "error while parsing json"
+              << "\n";
   }
 
-  const char* api_key_env = std::getenv("api_key");
-  std::string api_key_config;
-
-  std::string api_key;
-
-  api_key = api_key_env;
-  if (!api_key_env) {
-    std::cout << "api_key not found in .env\n";
-
-    api_key_config = f["api_key"].get<std::string>();
-    if (api_key_config.empty()) {
-      std::cout << "api_key not found in config.json\n";
-      return 2;
-    } 
+  std::string api_key = f["api_key"].get<std::string>();
+  if (api_key.empty()) {
+    std::cout << "api_key not found in config.json\n";
+    return 2;
   }
-  api_key = api_key_config;
-
 
   std::vector<std::string> cities = f["cities"];
 
